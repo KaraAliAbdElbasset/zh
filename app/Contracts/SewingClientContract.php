6d145aba26d2e4ff1658bc0d66de72462ae90310
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-05-09 01:46:29 +0100

    create: SewingClient repos and contract

---
+++ app/Contracts/SewingClientContract.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Contracts;
+
+
+interface SewingClientContract extends BaseContract
+{
+
+}
---
+++ app/Repositories/SewingClientRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Models\SewingClient;
+
+class SewingClientRepository extends BaseRepository implements \App\Contracts\SewingClientContract
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function findOneById($id, array $relations = [])
+    {
+        return SewingClient::with($relations)->findOrFail($id);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findByFilter(int $per_page = 10, array $relations = [], array $scopes = [])
+    {
+        $query = SewingClient::with($relations)->scopes($scopes)->newQuery();
+        return $this->applyFilter($query,$per_page);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function add(array $data)
+    {
+        return SewingClient::create($data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update(int $id, array $data)
+    {
+        $sc = $this->findOneById($id);
+        $sc->update($data);
+        return $sc;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(int $id)
+    {
+        return SewingClient::destroy($id);
+    }
+}
