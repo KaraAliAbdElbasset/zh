12440b4ed41442cd3d9bf64e28caf0fe3b8cd37c
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-05-09 01:26:03 +0100

    update: Invoice Repos and Contract

--- app/Contracts/InvoiceContract.php
+++ app/Contracts/InvoiceContract.php
@@ -4,7 +4,7 @@
 namespace App\Contracts;
 
 
-interface InvoiceContract
+interface InvoiceContract extends BaseContract
 {
 
 }
--- app/Repositories/InvoiceRepository.php
+++ app/Repositories/InvoiceRepository.php
@@ -4,7 +4,51 @@
 namespace App\Repositories;
 
 
-class InvoiceRepository
+use App\Models\Invoice;
+
+class InvoiceRepository extends BaseRepository implements \App\Contracts\InvoiceContract
 {
 
+    /**
+     * @inheritDoc
+     */
+    public function findOneById($id, array $relations = [])
+    {
+        return Invoice::with($relations)->findOrFail($id);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findByFilter(int $per_page = 10, array $relations = [], array $scopes = [])
+    {
+        $query = Invoice::with($relations)->scopes($scopes)->newQuery();
+        return $this->applyFilter($query,$per_page);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function add(array $data)
+    {
+        return Invoice::create($data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update(int $id, array $data)
+    {
+        $invoice = $this->findOneById($id);
+        $invoice->update($data);
+        return $invoice;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(int $id)
+    {
+        return Invoice::destroy($id);
+    }
 }
