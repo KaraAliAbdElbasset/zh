beefa0021c8412c0ef31773039690921c3fd8a6f
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-05-12 01:17:30 +0100

    create: general Statistcs Contraoller

---
+++ app/Http/Controllers/GeneralStatisticController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\GeneralStatisticContract;
+use App\Http\Requests\GeneralStatisticRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class GeneralStatisticController extends Controller
+{
+    /**
+     * @var GeneralStatisticContract
+     */
+    protected $gs;
+
+    /**
+     * GeneralStatisticController constructor.
+     * @param GeneralStatisticContract $gs
+     */
+    public function __construct(GeneralStatisticContract $gs)
+    {
+        $this->gs = $gs;
+    }
+
+    public function index()
+    {
+        $general_statistics = $this->gs->findByFilter();
+        return view('general_statistics.index',compact('general_statistics'));
+    }
+
+    public function create()
+    {
+        return view('general_statistics.create');
+    }
+
+    /**
+     * @param GeneralStatisticRequest $request
+     * @return RedirectResponse
+     */
+    public function store(GeneralStatisticRequest $request): RedirectResponse
+    {
+        $this->gs->add($request->validated());
+        session()->flash('success',__('messages.create'));
+        return redirect()->route('general-statistics.index');
+    }
+
+    public function show($id)
+    {
+        $gs = $this->gs->findOneById($id);
+        return view('general_statistics.show',compact('gs'));
+    }
+
+
+    public function edit($id)
+    {
+        $gs = $this->gs->findOneById($id);
+        return view('general_statistics.edit',compact('gs'));
+    }
+
+    /**
+     * @param $id
+     * @param GeneralStatisticRequest $request
+     * @return RedirectResponse
+     */
+    public function update($id,GeneralStatisticRequest $request): RedirectResponse
+    {
+        $this->gs->update($id,$request->validated());
+        session()->flash('success',__('messages.update'));
+        return redirect()->route('general-statistics.index');
+    }
+
+    public function destroy($id)
+    {
+        $this->gs->delete($id);
+        session()->flash('success',__('messages.delete'));
+        return redirect()->route('general-statistics.index');
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     /**
      * UserContract instance
-     * @var
+     * @var UserContract
      */
     protected $u;
 
